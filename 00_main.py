1cb4987a51429e54017893a7a71c62f2fa94d6d9
Jomfo2 <136519825+Jomfo2@users.noreply.github.com>
2024-06-03 10:31:35 +1200

    Add files via upload

--- 00_main.py
+++ 00_main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Converter:
+    def __init__(self):
+        print("Hello World!")
 
 
-# Press the green button in the gutter to run the script.
+# main routine
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    root = Tk()
+    root.title("Measurement Converter")
+    root.geometry("500x550")
+    Converter()
+    root.mainloop()
